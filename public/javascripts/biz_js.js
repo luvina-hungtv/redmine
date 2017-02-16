089f3dc2bf98d05813c229acd29fba1bb6a85be6
luvina-hungtv <tranviethung@luvina.net>
2017-02-16 10:01:02 +0700

    Fix #260 - Sua lai template & button

--- public/javascripts/biz_js.js
+++ public/javascripts/biz_js.js
@@ -3,9 +3,9 @@
 /* Count tracker in show issue page, ex: Bug, Q&A, Bug review code, ... */
 function countTracker() {
   //trackers allow to count subtasks
-  var ticketAllow = ["5", "6", "7", "8", "10", "11"];
+  var ticketAllow = ["5", "6", "7", "8", "10"];
   //trackers counted
-  var trackerCount = {1: "Bug", 2: "Q&A", 3: "Bug Review Code", 4: "Bug Review TC", 5: "Bug UAT"};
+  var trackerCount = {1: "Bug test", 2: "Q&A", 3: "Bug Review Code", 4: "Bug Review TC", 5: "Bug UAT"};
 
   var trackerId = $("#trackerId").val();
   var staText = [];
@@ -13,9 +13,9 @@ function countTracker() {
     $.each(trackerCount, function(key, value) {
       //var c = $("#issue_tree").find("tr.tracker-" + key).length;//include Rejected status
       var c = countValidStatus(key);//ignore Rejected status
-			if (c > 0) {
+			//if (c > 0) {
         staText.push(value + ": " + c);
-      }
+      //}
     });
     if (staText.length > 0) {
     $("#statistical").html("Statistic: " + staText.join(", ")).css({"display":"", "color":"red", "font-weight":"bold"});
